5f2cd329d9f21efcec699b66dd8e6907bda0d7f0
shantum <shantumgupta@gmail.com>
2016-12-01 15:49:51 +0100

    updated home controller and routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if user_signed_in?
+      if UserLink.count > 3
+        @recent_links = UserLink.order(created_at: :desc).last(4)
+      else
+        @recent_links = UserLink.all.reverse_order
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  get '/user_links/new', to: 'user_links#new'
+  get '/user_links/view/:id', to: 'user_links#view', as: 'user_link_view'
+  get '/user_links/edit/:id', to: 'user_links#edit', as: 'user_link_edit'
+  get '/user_links/index', to: 'user_links#index'
+
+  post '/user_links/create', to: 'user_links#create'
+  post '/user_links/edit/:id', to: 'user_links#edit'
+  post '/user_links/delete/:id', to: 'user_links#delete', as: 'user_link_delete'
+
+
 end
